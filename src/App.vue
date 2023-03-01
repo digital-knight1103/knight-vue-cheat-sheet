84f1aed70a9a75bebe6993dd7017ccb9cbdb7923
„Paweł <bokszczanin.p@gmail.com>
2023-03-01 12:31:43 +0100

    change icon

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" /> -->
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" />
 
       <TheNavigation/>
 
