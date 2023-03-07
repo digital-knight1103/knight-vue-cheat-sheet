5bdc3ebaf989b586c27ed62b0d67c958fb36cc1f
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-03-07 09:59:20 +0100

    test avatar

--- src/components/atoms/TheAvatar/TheAvatar.vue
+++ src/components/atoms/TheAvatar/TheAvatar.vue
@@ -1,7 +1,7 @@
 <template>
-  <span class="relative inline-block flex-shrink-0">
+  <span class="relative inline-block">
     <img v-if="path.length > 0" :class="imgClass" :src="path" :alt="alt" />
-    <img v-else src="../../../assets/images/avatar.svg"/>
+    <img v-else src="../../../assets/images/av.png" :class="imgClass"/>
   </span>
 </template>
 
@@ -27,43 +27,35 @@ export default defineComponent({
   },
   setup(props) {
     const style = {
-      img: 'rounded-lg',
-      span: 'inline-flex items-center justify-center rounded-full border border-blue-medium'
+      img: 'rounded-lg bg-gray-600 p-1',
+      // span: 'inline-flex items-center justify-center rounded-full border border-blue-medium'
     }
     switch (props.size) {
       case 'xs':
         style.img += ' w-6 h-6'
-        style.span += ' w-6 h-6'
         break
       case 'sm':
         style.img += ' w-8 h-8'
-        style.span += ' w-8 h-8'
         break
       case 'md':
         style.img += ' w-10 h-10'
-        style.span += ' w-10 h-10'
         break
       case 'lg':
         style.img += ' w-12 h-12'
-        style.span += ' w-12 h-12'
         break
       case 'xl':
         style.img += ' w-14 h-14'
-        style.span += ' w-14 h-14'
         break
       case '2xl':
         style.img += ' w-28 h-28'
-        style.span += ' w-28 h-28'
         break
       default:
         break
     }
     const imgClass = computed(() => style.img)
-    const spanClass = computed(() => style.span)
 
     return {
-      imgClass,
-      spanClass
+      imgClass
     }
   }
 })
--- src/router/index.ts
+++ src/router/index.ts
@@ -39,6 +39,14 @@ const router = createRouter({
       component: () => import('../views/TabsView.vue')
     },
     {
+      path: '/avatar',
+      name: 'avatar',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/TheAvatarView.vue')
+    },
+    {
       path: '/atomicdesign',
       name: 'atomicdesign',
       // route level code-splitting
