eb4170099ace948c6ddd35d0c50e9c094e6f7e99
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-03-14 17:06:01 +0100

    add class anim navigation

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -273,29 +273,131 @@
 
     <div class="mx-auto lg:ml-[290px] rounded-lg mt-8">
       <RouterView v-slot="{ Component }">
-        <Transition name="fade" mode="out-in">
+        <!-- ref value name -->
+        <Transition :name="anim" mode="out-in">
           <Component :is="Component"/>
         </Transition>
       </RouterView>
     </div>
 
-    <div class="check ml-[600px] mx-auto">
+    <!-- <div class="check ml-[600px] mx-auto mt-10">
       <label for="">
         <input type="radio" name="anim" value="fade" v-model="anim">Fade
       </label>
       <label for="">
-        <input type="radio" name="anim" value="fade" v-model="anim">Slide
+        <input type="radio" name="anim" value="slide" v-model="anim">Slide
       </label>
       <label for="">
-        <input type="radio" name="anim" value="fade" v-model="anim">Slide down
+        <input type="radio" name="anim" value="slide-down" v-model="anim">Slide down
       </label>
       <label for="">
-        <input type="radio" name="anim" value="fade" v-model="anim">Grow in
+        <input type="radio" name="anim" value="grow-in" v-model="anim">Grow in
       </label>
       <label for="">
-        <input type="radio" name="anim" value="fade" v-model="anim">Grow out
+        <input type="radio" name="anim" value="grow-out" v-model="anim">Grow out
       </label>
+    </div> -->
 
+    <div class="check ml-[600px] mx-auto mt-10">
+      <!-- Fade -->
+      <div class="flex items-center">
+        <label
+          class="relative flex cursor-pointer items-center rounded-full p-3"
+          for="html"
+          data-ripple-dark="true"
+        >
+          <input
+            id="html"
+            name="anim"
+            type="radio"
+            value="fade"
+            v-model="anim"
+            class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-pink-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-pink-500 checked:before:bg-pink-500 hover:before:opacity-10"
+          />
+          <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-pink-500 opacity-0 transition-opacity peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3.5 w-3.5"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+            >
+              <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+            </svg>
+          </div>
+        </label>
+        <label
+          class="mt-px cursor-pointer select-none font-semibold text-white"
+          for="html"
+        >
+          Fade
+        </label>
+      </div>
+      <!-- Slide -->
+      <div class="inline-flex items-center">
+        <label
+          class="relative flex cursor-pointer items-center rounded-full p-3"
+          for="react"
+          data-ripple-dark="true"
+        >
+          <input
+            id="react"
+            name="anim"
+            type="radio"
+            value="slide"
+            v-model="anim"
+            class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-green-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-green-400 checked:before:bg-green-500 hover:before:opacity-10"
+          />
+          <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-green-500 opacity-0 transition-opacity peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3.5 w-3.5"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+            >
+              <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+            </svg>
+          </div>
+        </label>
+        <label
+          class="mt-px cursor-pointer select-none font-semibold text-white"
+          for="react"
+        >
+          Slide
+        </label>
+      </div>
+      <!-- Grow -->
+      <div class="flex items-center">
+        <label
+          class="relative flex cursor-pointer items-center rounded-full p-3"
+          for="html"
+          data-ripple-dark="true"
+        >
+          <input
+            id="html"
+            name="anim"
+            type="radio"
+            value="grow-in"
+            v-model="anim"
+            class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-blue-gray-200 text-blue-500 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-blue-500 checked:before:bg-blue-500 hover:before:opacity-10"
+          />
+          <div class="pointer-events-none absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-blue-500 opacity-0 transition-opacity peer-checked:opacity-100">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-3.5 w-3.5"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+            >
+              <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+            </svg>
+          </div>
+        </label>
+        <label
+          class="mt-px cursor-pointer select-none font-semibold text-white"
+          for="html"
+        >
+          Grow
+        </label>
+      </div>
     </div>
 
     <!-- TODO JESZCZE SPRAWDZIC porównać z app -->
@@ -305,7 +407,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import IconOverview from '../assets/icons/IconOverview.vue'
 import IconBurger from '../assets/icons/IconBurger.vue'
 import IconAnalitics from '@/assets/icons/IconAnalitics.vue'
@@ -334,6 +436,8 @@ export default defineComponent({
     IconDoubleArrowTop
   },
   setup() {
+    const anim = ref('fade')
+
     // Burger menus
     document.addEventListener('DOMContentLoaded', function() {
     // open
@@ -371,14 +475,15 @@ export default defineComponent({
                     menu[j].classList.toggle('hidden');
                 }
             });
+          }
         }
-    }
-});
+      });
+    
+      return {
+        anim,
+      }
   }
 })
-
-
-
 </script>
 
 <style scoped>
@@ -409,6 +514,7 @@ nav a:first-of-type {
   border: 8px;
 }
 
+/* Fade animation */
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
@@ -417,6 +523,54 @@ nav a:first-of-type {
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s ease;
+}
+
+/* Slide animation */
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-100%);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: 0.3s ease;
+}
+
+/* Slide-down animation */
+.slide-down-enter-from,
+.slide-down-leave-to {
+  opacity: 0;
+  transform: translateY(300px);
+}
+
+.slide-down-enter-active,
+.slide-down-leave-active {
+  transition: 0.3s ease;
+}
+
+/* Grow-in animation */
+.grow-in-enter-from,
+.grow-in-leave-to {
+  opacity: 0;
+  transform: scale(0.5);
+}
+
+.grow-in-enter-active,
+.grow-in-leave-active {
+  transition: 0.3s ease;
+}
+
+/* Grow-out animation */
+.grow-out-enter-from,
+.grow-out-leave-to {
+  opacity: 0;
+  transform: scale(1.5);
+}
+
+.grow-out-enter-active,
+.grow-out-leave-active {
+  transition: 0.3s ease;
 }
 
 </style>
