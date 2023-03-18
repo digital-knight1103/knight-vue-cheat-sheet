08281eebde71a049ddbc9750244e9ae6315e7fbe
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-03-18 21:40:06 +0100

    init form

---
+++ src/components/atoms/Form/FormEmail/FormEmail.vue
@@ -0,0 +1,22 @@
+<template>
+  <input
+    type="email"
+    :size="size"
+  />
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+// import FormText from '@/atoms/Form/FormEmail/FromEmail.vue'
+
+export default defineComponent({
+  name: 'FormEmail',
+  // components: { FormText },
+  props: {
+    size: {
+      type: String,
+      default: 'md',
+      validator: (v: string) => ['xs', 'sm', 'md', 'lg', 'xl'].includes(v)
+    }
+  }
+})
+</script>
--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -1,20 +1,26 @@
 <template>
   <div class="container">
-    <PageHeader title="Formularze"></PageHeader>
-    <PageContent></PageContent>
+    <PageHeader path="" title="Formularze"></PageHeader>
+    <PageContent>
+      https://vueschool.io/articles/vuejs-tutorials/building-a-multi-step-form-with-petite-vue/
+      <FormEmail size="lg"/>
+
+    </PageContent>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import PageHeader from '@/components/molecules/PageHeader/PageHeader.vue';
+import PageHeader from '@/components/molecules/PageHeader/PageHeader.vue'
 import PageContent from '@/components/molecules/PageContent/PageContent.vue'
+import FormEmail from '@/components/atoms/Form/FormEmail/FormEmail.vue'
 
 export default defineComponent({
   name: 'FormsView',
   components: {
     PageHeader,
-    PageContent
+    PageContent,
+    FormEmail,
   },
   setup() {
   }
