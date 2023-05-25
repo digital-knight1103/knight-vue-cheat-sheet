1d4a74cd1c392e798bf9ae732f65cf333c3aa49d
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-05-25 13:23:51 +0200

    add netlify configure file

---
+++ src/layouts/404.vue
@@ -0,0 +1,20 @@
+<template>
+  <main p="x4 y10" text="center teal-700 dark:gray-200">
+    <div text-4xl>
+      <div i-carbon-warning inline-block />
+    </div>
+    <RouterView />
+    <div>
+      <button btn text-sm m="3 t8" @click="router.back()">
+        {{ t('button.back') }}
+      </button>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+</script>
