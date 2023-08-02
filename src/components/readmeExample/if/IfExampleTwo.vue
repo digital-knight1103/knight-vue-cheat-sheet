0224635e7e9cab8bbf2182401618ea886df41106
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-08-02 10:14:44 +0200

    example

--- src/components/readmeExample/if/IfExampleTwo.vue
+++ src/components/readmeExample/if/IfExampleTwo.vue
@@ -1,9 +1,14 @@
-<script setup>
+<script setup lang="ts">
 import { reactive } from 'vue'
 
-const product = reactive({
-  name: 'Miecz',
-  inStock: false
+const fireSword = reactive({
+  name: 'Miecz ognia',
+  inventory: 0,
+})
+
+const airSword = reactive({
+  name: 'Miecz powietrza',
+  inventory: 12,
 })
 
 
@@ -11,13 +16,36 @@ const product = reactive({
 
 <template>
   <div class="p-4 mt-4 mb-8 border rounded-lg flex flex-col justify-center max-w-md mx-auto">
-    <h1> {{ product.name }}</h1>
-    <img class='w-8 h-8' src='../../../assets/images/avatar.svg' />
-    <p v-if="product.inStock">In stock</p>
-    <p v-else>Out of stock</p>
+
+    <div class="flex">
+      <img class='m-2 basis-1/5 h-16' src='../../../assets/images/fireSword.svg' />
+      <div class="p-2 grow">
+        <h1 class="text-white uppercase "> {{ fireSword.name }}</h1>
+        <p v-if="fireSword.inventory > 5">Duża dostępność</p>
+        <p v-else-if="fireSword.inventory <=5 && fireSword.inventory > 0">Mała dostepność</p>
+        <p v-else>Niedostępnę</p>
+        <span class="text-pink-500">{{ fireSword.inventory }}</span>
+      </div>
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-lg shadow h-12">
+        Kup
+      </button>
+    </div>
+
+    <div class="flex">
+      <img class='m-2 basis-1/5 h-16' src='../../../assets/images/airSword.svg' />
+      <div class="p-2 grow">
+        <h1 class="text-white uppercase "> {{ airSword.name }}</h1>
+        <p v-if="airSword.inventory > 5">Duża dostępność</p>
+        <p v-else-if="airSword.inventory <=5 && airSword.inventory > 0">Mała dostepność</p>
+        <p v-else>Niedostępnę</p>
+        <span class="text-green-500">{{ airSword.inventory }}</span>
+      </div>
+      <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-lg shadow h-12">
+        Kup
+      </button>
+    </div>
   </div>
 
-  <h1 v-show="product.inStock">v-show</h1>
 </template>
 
 
