1bf57715b77e24592564754aeacb8625c4d34a2c
Paweł Bokszczanin <bokszczanin.p@gmail.com>
2023-08-08 11:15:10 +0200

    content list

---
+++ src/components/readmeExample/list/ArrayExample.vue
@@ -0,0 +1,170 @@
+<template>
+  <div class="shopping-list">
+    <h1 class="text-2xl uppercase text-yellow-500"> {{meal}} </h1>
+    <!-- <ul>
+      <li>1kg sera</li>
+      <li>Sos pomidorowy</li>
+      <li>Szyneczka</li>
+      <li>Duży ananas</li>
+    </ul> -->
+    <ul>
+      <li
+        v-for="(item, index) in ingredients" 
+        :key="item"> {{ item }} | indeks: {{ index }} </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const meal = ref('Pizza z Ananasem')
+
+  const ingredients = ref([
+    "1kg sera",
+    "Sos pomidorowy",
+    "Szyneczka",
+    "Duży ananas",
+  ])
+</script>
+
+<style scoped>
+.counter {
+  font-size: 0.8rem;
+  padding-left: 10px;
+  padding-right: 10px;
+}
+
+.shopping-list {
+  background: #fff;
+  padding: 2rem;
+  margin: 2rem auto;
+  border-radius: 3px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.12), 0 2px 4px 0 rgba(0, 0, 0, 0.08);
+  width: 70%;
+  max-width: 900px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+a {
+  color: #6cb2eb;
+  font-size: 1.25rem;
+  transition: all 0.1s ease-in;
+  margin-top: 0.5rem;
+  display: block;
+}
+
+a:hover {
+  color: #3490dc;
+}
+
+li,
+p {
+  display: flex;
+  align-items: center;
+  line-height: 1.75;
+  letter-spacing: 0.5px;
+  color: #3d4852;
+  font-size: 1.25rem;
+  cursor: pointer;
+  transition: all 0.1s ease-in;
+}
+
+li:hover {
+  color: #22292f;
+}
+
+li input {
+  margin: 0 0.5rem 0;
+}
+
+.shopping-list > input,
+.shopping-list > select {
+  width: 100%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.add-item-form,
+.header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.add-item-form input {
+  width: 70%;
+  border-radius: 3px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid #f1f5f8;
+  color: #606f7b;
+  padding: 0.5rem 0.75rem;
+  box-sizing: border-box;
+  font-size: 1rem;
+  letter-spacing: 0.5px;
+  margin: 0.5rem 0;
+}
+
+.btn {
+  border: none;
+  border-radius: 3px;
+  margin: auto 0;
+  padding: 0.5rem 0.75rem;
+  flex-shrink: 0;
+  cursor: pointer;
+  font-size: 0.9rem;
+  letter-spacing: 0.5px;
+  transition: all 0.1s ease-in;
+}
+
+.btn[disabled] {
+  background: #8795a1;
+}
+
+.btn[disabled]:hover {
+  background: #606f7b;
+}
+
+.btn-primary {
+  background: #6cb2eb;
+  color: #fff;
+}
+
+.btn-primary:hover {
+  background: #3490dc;
+}
+
+.btn-cancel {
+  background: #ef5753;
+  color: #fff;
+}
+
+.btn-cancel:hover {
+  background: #e3342f;
+  color: #fff;
+}
+
+.strikeout {
+  text-decoration: line-through;
+  color: #b8c2cc;
+}
+
+.strikeout:hover {
+  color: #8795a1;
+}
+
+.priority {
+  color: #de751f;
+}
+</style>
--- src/views/PageMD/ListMark.md
+++ src/views/PageMD/ListMark.md
@@ -1,7 +1,76 @@
 ---
-title: Renderowanie list
+title: Renderowanie list v-for
 ---
 
 <h1 class='text-white mb-10 mt-5 text-2xl uppercase text-center'>
-  Renderowanie list
+  Renderowanie listy v-for
 </h1>
+
+<TextBoxMD>
+  <h1 class="text-2xl font-semibold">
+    <span class='text-green-500 forn-bold'>#</span>  Wylistujmy to. 
+  </h1>
+  <p class='my-2'>
+    Listownie elementów jest niezwykle przydatne naszych aplikacjach. No bo jak inaczej sobie wyobrażasz wyświetlenie listy użytkowników albo produktów do naszej ulubionej pizzuni. No właśnie przydą nam z odległej galaktyki jakies tablice i obiekty i trzeba by to szybciutko pokazać.
+  </p>
+  <p class='my-2'>
+    I tu na białym koniu wjeżdża dyrektywa "v-for". Służy ona do renderowania listy elementów na podstawie otrzymanej tablicy lub obiektu. 
+  </p>
+  <p class='my-2'>
+    To co jak już o pizzunie chodzi to może wylistujmy potrzebne składniki, co ty na to. To zaczynamy wałkowanie ciasta.
+  </p>
+</TextBoxMD>
+
+```vue 
+<template>
+  <h1> {{meal}} </h1>
+  <!-- <ul>
+    <li>1kg sera</li>
+    <li>Sos pomidorowy</li>
+    <li>Szyneczka</li>
+    <li>Duży ananas</li>
+  </ul> -->
+  <ul>
+    <li 
+      v-for="(item, index) in ingredients" 
+      :key="index"
+    > 
+      {{ item }}: indeks {{ index }} 
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const meal = ref('Pizza z Ananasem')
+
+  const ingredients = ref([
+    "1kg sera",
+    "Sos pomidorowy",
+    "Szyneczka",
+    "Duży ananas",
+  ])
+</script>
+```
+
+<ArrayExample />
+
+<TextBoxMD>
+  <p class='my-2'>
+    No i mamy ładną listę naszych produktów. Czas przeanalizować kod krok po kroku:
+  </p>
+  <p class='my-2'>
+    1. Naszą tablicę albo obiekt możemy wylistować sobie mimo że nie są danymi reaktywnymi ale nic z nim nie zrobimy. W aplikacjach nasze dane zapewne skądś zostaną dostarczone i będziemy na nich pracować dlatego warto je opakować w jakiś ref albo reactive (obiekty) aby były reaktywne. <br>
+    2. Dyrektywa "v-for" wymaga specjalnej składni w postaci item in items, Gdzie items jest tablicą danych źródłowych natomiast item pojedyńczym elementem naszej tablicy, po którym następuje iteracja. <br>
+    3. V-for obsługuje również opcjonalny drugi alias dla indeksu naszego elementu
+  </p>
+</TextBoxMD>
+
+```vue 
+<template>
+  <li v-for="(item, index) in items">
+    {{ item }} - {{ index }}
+  </li>
+</template>
+```
